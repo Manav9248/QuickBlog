83bc1a0fa33a8fced37753e715278c479dea7bed
Manav <manav96544@gmail.com>
2025-08-21 23:13:29 +0530

    ready to commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
+
+function App() {
+  return (
+    <>
+      <div className="overflow-x-hidden overflow-y-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<Blog/>}/>
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
